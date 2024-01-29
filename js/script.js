faefebcd6a4b0ed0b212130609b86fae87dd0211
Tomasz Szczepanik <szczepaniktom@gmail.com>
2024-01-29 22:26:57 +0100

    Add compuerMovie

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-let playerMovie = `papier`;
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + playerMovie + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+    computerMove = 'kamień';
+} else if (randomNumber == 2){
+    computerMove = 'papier';
+} else if (randomNumber == 3){
+    computerMove = 'nożyce';
+}
